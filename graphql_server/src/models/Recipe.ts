6c4e9ab53445827d2a96b533a7b77a634833c493
tbjag <tbjagodits@gmail.com>
2023-05-05 11:45:09 -0400

    mutated

--- graphql_server/src/models/Recipe.ts
+++ graphql_server/src/models/Recipe.ts
@@ -1,5 +1,7 @@
+import { InputObjectRef } from "@pothos/core";
 import { builder } from "../builder";
 import { prisma } from "../db";
+import { Prisma } from "@prisma/client";
 
 // Define recipe type
 builder.prismaObject("Recipe", {
@@ -11,8 +13,8 @@ builder.prismaObject("Recipe", {
     favoriteBy: t.relation("favoritedBy"),
     createdAt: t.expose("createdAt", { type: "Date" }),
     updatedAt: t.expose("updatedAt", { type: "Date" }),
-    ingredients: t.relation('ingredients'),
-    steps: t.exposeStringList('steps')
+    ingredients: t.relation("ingredients"),
+    steps: t.exposeStringList("steps"),
   }),
 });
 
@@ -21,7 +23,7 @@ builder.queryField("getAllRecipes", (t) =>
   t.prismaField({
     type: ["Recipe"],
     resolve: async (query, root, args, ctx, info) => {
-      return await prisma.recipe.findMany({...query});
+      return await prisma.recipe.findMany({ ...query });
     },
   })
 );
@@ -29,7 +31,7 @@ builder.queryField("getAllRecipes", (t) =>
 // Gets a single recipe by its Id
 builder.queryFields((t) => ({
   getRecipe: t.prismaField({
-    type: 'Recipe',
+    type: "Recipe",
     args: {
       id: t.arg.string({ required: true }),
     },
@@ -44,28 +46,40 @@ builder.queryFields((t) => ({
   }),
 }));
 
+const IngredientInput = builder.inputType("IngredientInput", {
+  fields: (t) => ({
+    name: t.string({ required: true }),
+    amount: t.float({ required: true }),
+    unitType: t.string({ required: true }),
+    unit: t.string({ required: true }),
+  }),
+});
+
+const RecipeInput = builder.inputType("RecipeInput", {
+  fields: (t) => ({
+    title: t.string({ required: true }),
+    course: t.string({ required: true }),
+    cuisineId: t.string({ required: true }),
+    authorId: t.string({ required: true }),
+    ingredients: t.field({ type: [IngredientInput], required: true }),
+    steps: t.stringList({ required: true }),
+  }),
+});
+
+//const RecipeCreate = InputObjectRef<Prisma.Recipe
+
 // Creates recipe
 builder.mutationFields((t) => ({
   createRecipe: t.prismaField({
-    type: 'Recipe',
+    type: "Recipe",
     args: {
-      title: t.arg.string({required:true}),
-      course: t.arg.string({required:true}),
-      cuisineId: t.arg.string({required:true}),
-      authorId: t.arg.string({required:true}),
-      //ingredients: t.arg.
-      steps: t.arg.stringList({required:true})
+      input: t.arg({ type: RecipeInput, required: true }),
     },
-    resolve: (query, root, args, ctx, info) => {
-      return prisma.recipe.create({
+    resolve: async (query, root, args, ctx, info) => {
+      return await prisma.recipe.create({
         ...query,
-        data: {
-          title: args.title,
-          course: args.course,
-          cousineId: args.cuisineId,
-          authorId: args.authorId,
-          steps: args.steps
-        },
+        data: args.input,
+        include: { ingredients: true },
       });
     },
   }),
