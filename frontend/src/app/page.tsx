c95287615569e05ede1edbf9416a07c9ebb39b7a
turdis <thindsbhs@gmail.com>
2023-06-16 13:24:01 -0400

    front end changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -57,48 +57,50 @@ export default function Recipes(): JSX.Element {
   
 
   return (
+  <div className="container mx-auto sm:w-11/12 md:w-3/4 lg:w-1/2 xl:w-1/3 p-10 m-10">
     <div>
       <Head>
-        <title className= "font-bold mb-3 text-4x2">Recipes</title>
+        <title className="font-bold mb-3 text-4xl">Recipes</title>
       </Head>
       <div className="container mx-auto p-10 m-10">
         <div className="flex flex-col">
-          <h1 className="font-bold mb-3 text-2xl">Recipe Title
-            <textarea
-              value={recipe.title}
-              onChange={(e) => handleChange(e, 'title')}
-              className="border-2 p-2 rounded-md text-black"
-            ></textarea>
-          </h1>
-          <h1 className="font-bold mb-3 text-2xl">Ingredients
-            <textarea
-              value={recipe.ingredients}
-              onChange={(e) =>handleChange(e, 'ingredients')}
-              className="border-2 p-2 rounded-md text-black"
-            ></textarea>
-          </h1>
-          <h1 className="font-bold mb-3 text-2xl">Instructions
-            <textarea
-              value={recipe.instructions}
-              onChange={(e) => handleChange(e, 'instructions')}
-              className="border-2 p-2 rounded-md text-black"
-            ></textarea>
-          </h1>
-          <h1 className="font-bold mb-3 text-2xl">Notes
-            <textarea
-              value={recipe.notes}
-              onChange={(e) => handleChange(e, 'notes')}
-              className="border-2 p-2 rounded-md text-black"
-            ></textarea>
-          </h1>
+          <h1 className="font-bold mb-3 text-2xl">Recipe Title</h1>
+          <textarea
+            value={recipe.title}
+            onChange={(e) => handleChange(e, 'title')}
+            className="border-2 p-2 rounded-md text-black mb-3"
+          ></textarea>
+
+          <h1 className="font-bold mb-3 text-2xl">Ingredients</h1>
+          <textarea
+            value={recipe.ingredients}
+            onChange={(e) => handleChange(e, 'ingredients')}
+            className="border-2 p-2 rounded-md text-black mb-3"
+          ></textarea>
+
+          <h1 className="font-bold mb-3 text-2xl">Instructions</h1>
+          <textarea
+            value={recipe.instructions}
+            onChange={(e) => handleChange(e, 'instructions')}
+            className="border-2 p-2 rounded-md text-black mb-3"
+          ></textarea>
+
+          <h1 className="font-bold mb-3 text-2xl">Notes</h1>
+          <textarea
+            value={recipe.notes}
+            onChange={(e) => handleChange(e, 'notes')}
+            className="border-2 p-2 rounded-md text-black mb-3"
+          ></textarea>
+
           <div className="mx-auto p-3 mt-5">
             <button
               onClick={handleSubmit}
-              className="bg-green-500 p-3 text-white rounded-md"
+              className="bg-green-500 hover:bg-green-600 p-3 text-white rounded-md transition-colors duration-300"
             >
-              Submit
+            Submit
             </button>
           </div>
+
           <div>
             <ul>
               {recipes &&
@@ -107,7 +109,20 @@ export default function Recipes(): JSX.Element {
                     key={r.id}
                     className="bg-yellow-100 my-3 py-3 px-4 border-yellow-200 border-2 rounded-md text-black"
                   >
-                    Title: {r.title}, Ingredients: {r.ingredients}, Instructions: {r.instructions}, Notes: {r.notes}
+                    <div>
+                      <span className="font-bold">Title:</span> {r.title}
+                    </div>
+                    <div>
+                      <span className="font-bold">Ingredients:</span>{' '}
+                      {r.ingredients}
+                    </div>
+                    <div>
+                      <span className="font-bold">Instructions:</span>{' '}
+                      {r.instructions}
+                    </div>
+                    <div>
+                      <span className="font-bold">Notes:</span> {r.notes}
+                    </div>
                   </li>
                 ))}
             </ul>
@@ -115,7 +130,8 @@ export default function Recipes(): JSX.Element {
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 Recipes['useClient'] = true; // Marking the Notes component as a client entry
