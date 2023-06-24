d6c2699bb5b79389af4953146907d01d2f755e5d
tbjag <tbjagodits@gmail.com>
2023-06-24 19:39:11 -0400

    refactored crud user

--- backend/app/crud/crud_user.py
+++ backend/app/crud/crud_user.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, List, Union, Optional
  
 from sqlalchemy.orm import Session
 
@@ -7,11 +7,7 @@ from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
 from app.core.security import get_password_hash
 
-
-class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
-    def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
-        return db.query(User).filter(User.email == email).first()
-
+class CRUDUser:
     def create(self, db: Session, *, obj_in: UserCreate) -> User:
         create_data = obj_in.dict()
         create_data.pop("password")
@@ -19,9 +15,8 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         db_obj.hashed_password = get_password_hash(obj_in.password)
         db.add(db_obj)
         db.commit()
-
         return db_obj
-
+    
     def update(
         self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
     ) -> User:
@@ -31,9 +26,23 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             update_data = obj_in.dict(exclude_unset=True)
 
         return super().update(db, db_obj=db_obj, obj_in=update_data)
+    
+    def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
+        return db.query(User).filter(User.email == email).first()
+
+    def get(self, db: Session, id: Any) -> User:
+        return db.query(User).filter(User.id == id).first()
+    
+    def get_multi(self, db: Session, *, skip: int = 0, limit: int = 5000) -> List[User]:
+        return db.query(User).order_by(User.id).offset(skip).limit(limit).all()
 
     def is_superuser(self, user: User) -> bool:
         return user.is_superuser
+    
+    def remove(self, db: Session, *, id: int) -> User:
+        obj = db.query(User).get(id)
+        db.delete(obj)
+        db.commit()
+        return obj
 
-
-user = CRUDUser(User)
+user = CRUDUser()
